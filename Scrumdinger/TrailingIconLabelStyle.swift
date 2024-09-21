40f97165c5ba0bc4e5e32c503288b7f4df1b8569
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-09-21 11:45:48 -0300

    ✨ feat: implement DailyScrum model and CardView UI
    - Create DailyScrum struct with properties for title, attendees, length, and theme.
    - Add sample data for testing and preview purposes.
    - Develop CardView to display scrum details, including title, attendee count, and meeting length.
    - Introduce custom TrailingIconLabelStyle for better label presentation.
    - Update Asset catalog with themes for visual consistency.

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by João Carlos Magalhães on 21/09/24.
+//
+
+import SwiftUI
+
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
