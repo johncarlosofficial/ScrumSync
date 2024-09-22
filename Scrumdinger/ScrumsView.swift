981dd656e687f39e3e9cab7799848428613c672d
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-09-22 07:37:33 -0300

    ✨ feat: add `Identifiable` protocol to `DailyScrum` model
    - Introduced an `id` property of type UUID to the DailyScrum struct.
    - Updated initializer to accept and set the id.
    - Added a new ScrumsView.swift file for the Scrum view.

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by João Carlos Magalhães on 22/09/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
