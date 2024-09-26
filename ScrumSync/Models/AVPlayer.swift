19c535ee70f12e2eef3fa1c91117c02a0a440945
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-09-26 20:46:22 -0300

    ✨ feat: implement `ScrumTimer` model and play audio with AVFoundation

---
+++ ScrumSync/Models/AVPlayer.swift
@@ -0,0 +1,16 @@
+//
+//  AVPlayer.swift
+//  ScrumSync
+//
+//  Created by John Carlos on 26/09/24.
+//
+
+import Foundation
+import AVFoundation
+
+extension AVPlayer {
+    static let sharedDingPlayer: AVPlayer = {
+        guard let url = Bundle.main.url(forResource: "ding", withExtension: "wav") else { fatalError("Failed to find sound file.") }
+        return AVPlayer(url: url)
+    }()
+}
