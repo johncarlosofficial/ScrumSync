bd459a9f47fe0a655a67ea555a543b16e176c1f8
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2024-10-01 08:47:23 -0300

    ✨ feat: implement error handling

---
+++ ScrumSync/Models/ErrorWrapper.swift
@@ -0,0 +1,21 @@
+//
+//  ErrorWrapper.swift
+//  ScrumSync
+//
+//  Created by John Carlos on 01/10/24.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
